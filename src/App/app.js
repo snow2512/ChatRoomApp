a26bc8c01d6de622d21735fe7027b470f624e68e
cywang22 <cywang22@iflytek.com>
2021-10-09 18:02:28 +0800

    feat: 新增 antd, css, sass 处理

--- src/App/app.js
+++ src/App/app.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import { Button } from 'antd'
+import 'antd/dist/antd.css'
 class App extends React.Component {
   render() {
     return (
       <div>
         <h1>ChatRoom App</h1>
+        <Button type="primary">button</Button>
       </div>
     )
   }
