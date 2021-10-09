ec897017dae733687936dd96ec53c9a702dce47e
cywang22 <cywang22@iflytek.com>
2021-10-09 15:21:33 +0800

    feat: 美化 webpack 终端显示效果

--- .webpack/webpack.dev.js
+++ .webpack/webpack.dev.js
@@ -1,10 +1,27 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-
+const ProgressBarPlugin = require('progress-bar-webpack-plugin')
+const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin")
+const chalk = require('chalk')
+const port = 2512
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
+  stats: 'errors-only',
   devServer: {
     static: './dist',
+    port
   },
+  plugins: [
+    new FriendlyErrorsWebpackPlugin({
+      // compilationSuccessInfo : {
+      //   messages: [chalk.blueBright(`Project running at http://locahost:${port}`)]
+      // },
+      clearConsole: true
+    }),
+    new ProgressBarPlugin({
+      format: '  build [:bar] ' + chalk.green.bold(':percent') + ' (:elapsed seconds)',
+      clear: false,
+    }),
+  ]
 });
